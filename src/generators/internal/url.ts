1cff0d2667f7ebe9c9da2238acabd7e18a2ed7ce
Jesse Luoto <jesse.luoto@gmail.com>
2023-03-20 22:52:26 +0200

    Fix urlparams generator
    We cannot use top-level nested generators (pass a generated value to a
    generator).

--- src/generators/internal/url.ts
+++ src/generators/internal/url.ts
@@ -8,10 +8,4 @@ const PARAMS_LIST = "SecLists/Discovery/Web-Content/burp-parameter-names.txt";
 
 const urlparam = () => oneFrom(PARAMS_LIST);
 
-export const urlparams = (params: Record<string, string>) =>
-  map(
-    array(tuple(urlparam(), string(letter))),
-    (extraParams) =>
-      new URLSearchParams([...extraParams, ...Object.entries(params)]),
-    (v) => [...v.entries()].filter(([k]) => !Object.keys(params).includes(k))
-  );
+export const urlparams = () => array(tuple(urlparam(), string(letter)));
--- src/generators/url.ts
+++ src/generators/url.ts
@@ -1,5 +1,4 @@
 import { registerProperty } from "./internal/state/properties";
 import { urlparams as _urlparams } from "./internal/url";
 
-export const urlparams = (params: Record<string, string>) =>
-  registerProperty(() => _urlparams(params));
+export const urlparams = () => registerProperty(_urlparams);
